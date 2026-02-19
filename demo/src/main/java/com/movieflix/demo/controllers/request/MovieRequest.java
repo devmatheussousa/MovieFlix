e2afe0fe3ef72c73306b8e7600b3674dfab963ba
DevMatheusSousa <dev.matheus.contato@gmail.com>
2026-02-19 11:10:07 -0300

    feat: movie flix

--- demo/src/main/java/com/movieflix/demo/controllers/request/MovieRequest.java
+++ demo/src/main/java/com/movieflix/demo/controllers/request/MovieRequest.java
@@ -1,11 +1,14 @@
 package com.movieflix.demo.controllers.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record MovieRequest(
         String title,
         String description,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") // Para garantir que a data seja enviada no formato correto (ex: "2024-06-01")
         LocalDate releaseDate,
         Double rating,
         List<Long> categoriesIds,
--- demo/src/main/java/com/movieflix/demo/service/MovieService.java
+++ demo/src/main/java/com/movieflix/demo/service/MovieService.java
@@ -1,10 +1,13 @@
 package com.movieflix.demo.service;
 
+import com.movieflix.demo.entities.Category;
 import com.movieflix.demo.entities.Movie;
+import com.movieflix.demo.entities.Streaming;
 import com.movieflix.demo.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,12 +15,35 @@ import java.util.List;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie){
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll(){
         return movieRepository.findAll();
+    }
+
+    private List<Category> findCategories(List<Category> categories){
+
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(
+                category -> {
+                    categoryService.getCategoryById(category.getId()).ifPresent(categoriesFound::add);
+                });
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(
+                streaming -> {
+                    streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add);
+                });
+        return streamingsFound;
     }
 }
