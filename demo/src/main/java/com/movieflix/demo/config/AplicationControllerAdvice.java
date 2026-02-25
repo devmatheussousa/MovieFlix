3d1bca4f211e3acd297bc296c4ee3718c6073329
DevMatheusSousa <dev.matheus.contato@gmail.com>
2026-02-25 10:09:06 -0300

    feat: handler:

--- demo/src/main/java/com/movieflix/demo/config/AplicationControllerAdvice.java
+++ demo/src/main/java/com/movieflix/demo/config/AplicationControllerAdvice.java
@@ -2,9 +2,13 @@ package com.movieflix.demo.config;
 
 import com.movieflix.demo.exception.UsernameOrPasswordInvalidException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class AplicationControllerAdvice {
@@ -13,5 +17,17 @@ public class AplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleNotFoundException(UsernameOrPasswordInvalidException ex){
         return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
